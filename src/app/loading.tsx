fe32d32a511604b12039c07a815b404d73e228df
imarcowenzel <marcowenzelcirilo@gmail.com>
2023-12-14 18:57:04 -0300

    updating

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,12 @@
+import Container from "@/components/container";
 import Spinner from "@/components/spinner";
 
 const Loading = () => {
   return (
-    <main className="flex min-h-[calc(100dvh-497px)] w-full flex-col items-center justify-center gap-y-3 py-12 md:min-h-[calc(100dvh-321px)] lg:min-h-[calc(100dvh-256.98px)]">
+    <Container>
       <Spinner />
       <h1 className="text-2xl font-semibold">Loading</h1>
-    </main>
+    </Container>
   );
 };
 
