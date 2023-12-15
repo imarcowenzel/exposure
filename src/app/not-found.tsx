96290c3919c34e9ef84da2202f51d63554df3adb
imarcowenzel <marcowenzelcirilo@gmail.com>
2023-12-15 08:57:57 -0300

    Updating

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,27 @@
-import { CircleOff, XCircleIcon } from "lucide-react";
-import React from "react";
+"use client";
+
+import Container from "@/components/container";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+import { useRouter } from "next/navigation";
+import { FaRegFaceSadTear } from "react-icons/fa6";
 
 const NotFound = () => {
+  const router = useRouter();
   return (
-    <main className="flex gap-y-5 min-h-[calc(100svh-384px)] w-full flex-col items-center justify-center py-12 md:min-h-[calc(100svh-256px)]">
-      <CircleOff className="text-9xl" />
-      <h1 className="text-2xl font-semibold">Page Not Found :(</h1>
-    </main>
+    <Container className="h-[768px] md:h-0">
+      <div className="flex flex-col items-center justify-center gap-y-4">
+        <FaRegFaceSadTear className="text-4xl" />
+        <h1 className="text-2xl font-bold">Page not found!</h1>
+        <p
+          onClick={() => router.back()}
+          className={cn(buttonVariants({ variant: "link" }), "cursor-pointer")}
+        >
+          Go back.
+        </p>
+      </div>
+    </Container>
   );
 };
 
