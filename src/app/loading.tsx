889f906a0786cc6f18ae37607a01b630569a3983
imarcowenzel <marcowenzelcirilo@gmail.com>
2023-12-15 08:42:47 -0300

    Updating

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,8 +4,10 @@ import Spinner from "@/components/spinner";
 const Loading = () => {
   return (
     <Container>
-      <Spinner />
-      <h1 className="text-2xl font-semibold">Loading</h1>
+      <div className="flex flex-col items-center justify-center gap-y-6 md:gap-y-8">
+        <Spinner className="h-7 w-7 md:h-8 md:w-8" />
+        <h1 className="text-2xl font-bold md:text-3xl">Loading ...</h1>
+      </div>
     </Container>
   );
 };
